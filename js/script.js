b13852cbf9c39fd3de203db2d00f4f6678616d43
gilangap01 <harzthdev01@gmail.com>
2021-06-23 08:43:32 +0700

    Memperbaiki table dan memberi tombol reset

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-
-
 const board = document.querySelector('.board');
-let i = 1;
+const resetButton = document.querySelector('.reset');
+const tabel = document.querySelectorAll('th')
+let urutanJalan = 1;
+const player1 = 'X';
+const player2 = 'O';
 
-board.addEventListener('click', function(e){
-	if(i % 2 == 0){
-		e.target.textContent = 'O';
-	} else {
-		e.target.textContent = 'X';
-	}
-	i += 1;
+board.addEventListener('click', function(e) {
+    if (e.target.textContent == '') {
+        if (urutanJalan % 2 == 0) {
+            e.target.textContent = player2;
+        } else {
+            e.target.textContent = player1;
+        }
+        urutanJalan += 1;
+    }
 });
+
+resetButton.addEventListener('click', function(e) {
+    for (var i = 0; i < tabel.length; i++) {
+        tabel[i].textContent = '';
+        urutanJalan = 1;
+    }
+})
+
